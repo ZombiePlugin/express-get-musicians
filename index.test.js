daa37f081123649b533363a8f1919dcbc1fc7a38
M <muzameelahmed1@gmail.com>
2023-11-07 15:49:20 +0000

    REST CRUD methods and tests

--- index.test.js
+++ index.test.js
@@ -16,4 +16,36 @@ describe("./musicians endpoint", () => {
     const response = await request(app).get("/musicians");
     expect(response.statusCode).toBe(200);
   });
+  test("Find musician by id", async () => {
+    const response = await request(app).get("/musicians/1");
+    expect(response.statusCode).toBe(200);
+  });
+  test("Create new musician", async () => {
+    const createdGuy = { name: "Guy", instrument: "Guitar" };
+    const response = await request(app)
+      .post("/musicians")
+      .send(createdGuy)
+      .expect(200)
+      .expect((res) => {
+        res.body.name = "Guy";
+      });
+  });
+  test("Update musician by id", async () => {
+    const NewGuy = { name: "New Guy", instrument: "Violin" };
+    const response = await request(app)
+      .put("/musicians/4")
+      .send(NewGuy)
+      .expect(200)
+      .expect((res) => {
+        res.body.name = "New Guy";
+      });
+  });
+  test("Delete musician by id", async () => {
+    const response = await request(app)
+      .delete("/musicians/4")
+      .expect(200)
+      .expect((res) => {
+        res.body.name = "New Guy";
+      });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -7,9 +7,35 @@ const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/musicians", async (req, res) => {
   const allMusicians = await Musician.findAll();
   res.json(allMusicians);
 });
 
+app.get("/musicians/:id", async (req, res) => {
+  const foundMusician = await Musician.findByPk(req.params.id);
+  res.json(foundMusician);
+});
+
+app.post("/musicians", async (req, res) => {
+  const newMusician = await Musician.create(req.body);
+  res.json(newMusician);
+});
+
+app.put("/musicians/:id", async (req, res) => {
+  const updatedMusician = await Musician.update(req.body, {
+    where: { id: req.params.id },
+  });
+  res.json(updatedMusician);
+});
+
+app.delete("/musicians/:id", async (req, res) => {
+  const deletedMusician = await Musician.destroy({
+    where: { id: req.params.id },
+  });
+  res.json(deletedMusician);
+});
 module.exports = app;
